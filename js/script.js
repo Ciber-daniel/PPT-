1746de5cdd736dd1f77c49d4d54b357c28ca0233
Daniel <daniel.rodriguez.finol@gmail.com>
2021-02-24 06:29:40 -0500

    Refactor JS logic

--- js/script.js
+++ js/script.js
@@ -1,132 +1,133 @@
-window.onload = function () {
-
-}
 /** */
-const options = ['papel', 'piedra', 'tijera']
-const optionsImages = ['./img/papel.ico', './img/piedra.ico', './img/tijera.ico']
+const options = ["papel", "piedra", "tijera"];
+const optionsImages = [
+  "./img/papel.ico",
+  "./img/piedra.ico",
+  "./img/tijera.ico",
+];
 
-const tijeraBtn = document.getElementById('elemento-tijera');
-const papelBtn = document.getElementById('elemento-papel');
-const piedraBtn = document.getElementById('elemento-piedra');
+const optionsContainer = document.getElementsByClassName("container_space")[0];
+const htmlWrite = `
+            <div id="elemento-papel" data-index="1" class="icon_option">
+              <img src="./img/papel.ico" alt="" />
+            </div>
+            <br />
+            <div id="elemento-piedra" data-index="2" class="icon_option">
+              <img src="./img/piedra.ico" alt="" />
+            </div>
+            <br />
+            <div id="elemento-tijera" data-index="3" class="icon_option">
+              <img src="./img/tijera.ico" alt="" />
+            </div>
+          </div>`;
 
 async function randomizeSelection() {
-    let selectedIndex;
+  let selectedIndex;
 
-    selectedIndex = await passOptionsAndGetLastIndex();
+  selectedIndex = await passOptionsAndGetLastIndex();
 
-    return selectedIndex;
+  return selectedIndex;
 }
 
 async function wait(ms) {
-    return new Promise((resolve) => setTimeout(() => resolve(), ms));
+  return new Promise((resolve) => setTimeout(() => resolve(), ms));
 }
 
 async function passOptionsAndGetLastIndex() {
-    const times = 10;
-    let startingAt = Math.floor(Math.random() * options.length);
-    const machineElement = document.getElementById('machine_response');
+  const times = 20;
+  let startingAt = Math.floor(Math.random() * options.length);
+  const machineElement = document.getElementById("machine_response");
 
-    let currentIndex = startingAt;
-    for (let i = 0; i < times; i++) {
-        const currentUrl = optionsImages[currentIndex];
-        machineElement.innerHTML = `<div class="icon_option"><img src="${currentUrl}" alt=""></div>`;
-        await wait(100);
-        if (i < times - 1) {
-            if ((currentIndex + 1) === options.length) {
-                currentIndex = 0;
-            } else {
-                currentIndex++;
-            }
-        }
+  let currentIndex = startingAt;
+  for (let i = 0; i < times; i++) {
+    const currentUrl = optionsImages[currentIndex];
+    machineElement.innerHTML = `<div class="icon_option"><img src="${currentUrl}" alt=""></div>`;
+    await wait(100);
+    if (i < times - 1) {
+      if (currentIndex + 1 === options.length) {
+        currentIndex = 0;
+      } else {
+        currentIndex++;
+      }
     }
-    return currentIndex;
+  }
+  return currentIndex;
 }
 
-async function iniciarEvaluacion(itemIndex) {
-    const machineSelectedIndex = await randomizeSelection();
-    console.log('Machine selected index', machineSelectedIndex)
+async function startEvaluation(itemIndex) {
+  const machineSelectedIndex = await randomizeSelection();
 
-    const machineSelected = options[machineSelectedIndex];
-    console.log(machineSelected)
-    const personSelected = options[itemIndex];
-    console.log(personSelected)
-    if (personSelected === 'tijera') {
-        if (machineSelected === 'papel') {
-            swal(
-                'Buen trabajo!',
-                'Ganaste la partida!',
-                'success'
-            )
-        } else if (machineSelected === 'tijera') {
-            swal('empate',
-                'Has empatado',
-                'success'
-
-            )
-        } else {
-            swal(
-                'Loser!',
-                'Perdiste la partida!',
-                'error'
-            )
-        }
+  const machineSelected = options[machineSelectedIndex];
+  const personSelected = options[itemIndex];
+  if (personSelected === "tijera") {
+    if (machineSelected === "papel") {
+      swal("Buen trabajo!", "Ganaste la partida!", "success");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
+    } else if (machineSelected === "tijera") {
+      swal("empate", "Has empatado", "info");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
+    } else {
+      swal("Loser!", "Perdiste la partida!", "error");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
     }
-    if (personSelected === 'piedra') {
-        if (machineSelected === 'tijera') {
-            swal(
-                'Buen trabajo!',
-                'Ganaste la partida!',
-                'success'
-            )
-        } else if (machineSelected === 'piedra') {
-            swal('empate',
-                'Has empatado',
-                'success'
-            )
-        } else {
-            swal(
-                'Loser!',
-                'Perdiste la partida!',
-                'error'
-            )
-        }
+  }
+  if (personSelected === "piedra") {
+    if (machineSelected === "tijera") {
+      swal("Buen trabajo!", "Ganaste la partida!", "success");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
+    } else if (machineSelected === "piedra") {
+      swal("empate", "Has empatado", "info");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
+    } else {
+      swal("Loser!", "Perdiste la partida!", "error");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
     }
-    if (personSelected === 'papel') {
-        if (machineSelected === 'piedra') {
-            swal(
-                'Buen trabajo!',
-                'Ganaste la partida!',
-                'success'
-            )
-        } else if (machineSelected === 'papel') {
-            swal('empate',
-                'Has empatado',
-                'success'
-
-            )
-        } else {
-            swal(
-                'Loser!',
-                'Perdiste la partida!',
-                'error'
-            )
-        }
+  }
+  if (personSelected === "papel") {
+    if (machineSelected === "piedra") {
+      swal("Buen trabajo!", "Ganaste la partida!", "success");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
+    } else if (machineSelected === "papel") {
+      swal("empate", "Has empatado", "info");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
+    } else {
+      swal("Loser!", "Perdiste la partida!", "error");
+      optionsContainer.innerHTML = `${htmlWrite}`;
+      addListeners();
     }
+  }
 }
 
+function addListener(element, image, nameIdentificator) {
+  element.addEventListener("click", () => {
+    const index = parseInt(element.dataset.index) - 1;
+    optionsContainer.innerHTML = `<div id="elemento-${nameIdentificator}" data-index=${element.dataset.index} class="icon_option">
+      <img src=${image} alt=""/>
+    </div>`;
+    startEvaluation(index);
+  });
+}
 
-tijeraBtn.addEventListener('click', async function () {
-    const index = parseInt(tijeraBtn.dataset.index) - 1;
-    iniciarEvaluacion(index);
-})
+function addListeners() {
+  // buttons
+  const papelBtn = document.getElementById("elemento-papel");
+  const piedraBtn = document.getElementById("elemento-piedra");
+  const tijeraBtn = document.getElementById("elemento-tijera");
 
-papelBtn.addEventListener('click', async function () {
-    const index = parseInt(papelBtn.dataset.index) - 1;
-    iniciarEvaluacion(index);
-    console.log(index)
-})
+  // add listeners
+  addListener(papelBtn, optionsImages[0], options[0]);
+  addListener(piedraBtn, optionsImages[1], options[1]);
+  addListener(tijeraBtn, optionsImages[2], options[2]);
+}
 
-piedraBtn.addEventListener('click', async function () {
-    const index = parseInt(piedraBtn.dataset.index) - 1;
-    iniciarEvaluacion(index);
-})
+window.onload = function () {
+  addListeners();
+};
